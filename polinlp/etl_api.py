006597053835d935ce4e221517674f5e7647eef2
Romain-DengGueret <romain.dengueret@gmail.com>
2025-02-06 15:11:35 +0000

    data/prepro json to parquet

--- polinlp/etl_api.py
+++ polinlp/etl_api.py
@@ -1,4 +1,13 @@
 import requests  # type: ignore
+import polars as pl
+import os
+
+# Remonter d'un niveau
+parent_dir = os.getcwd() + "/PoliNLP"
+
+# Construire les chemins dynamiques
+data_path = parent_dir + "/data"
+prepro_data_path = data_path + "/prepro"
 
 # Defining endpoint
 drees = "https://data.drees.solidarites-sante.gouv.fr/api/explore/v2.1"
@@ -9,3 +18,17 @@ endpoint = drees + datasets + minimasociaux
 
 # Requests
 response = requests.get(endpoint).json()  # We get a json file
+
+# Convert all in str
+cleaned_response = [
+    {
+        key: str(value) if not isinstance(value, (int, float)) else value
+        for key, value in entry.items()
+    }
+    for entry in response
+]
+
+df = pl.DataFrame(cleaned_response)
+
+path = prepro_data_path + "/trajectoires_beneficiares_minimas_sociaux.parquet"
+df.write_parquet(path)
