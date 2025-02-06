9a7598d99c66e96facac77ec7887723ea45319da
Romain-DengGueret <romain.dengueret@gmail.com>
2025-02-06 21:23:20 +0000

    polinlp/etl_api.py : function for testing

--- polinlp/etl_api.py
+++ polinlp/etl_api.py
@@ -14,23 +14,40 @@ drees = "https://data.drees.solidarites-sante.gouv.fr/api/explore/v2.1"
 datasets = "/catalog/datasets/"
 minimasociaux = "/trajectoires-des-beneficiaires-de-minima-sociaux/exports/json"
 
-endpoint = drees + datasets + minimasociaux
+url = drees + datasets + minimasociaux
 
-# Requests
-response = requests.get(endpoint).json()  # We get a json file
 
-# Convert all in str
-cleaned_response = [
-    {
-        key: str(value) if not isinstance(value, (int, float)) else value
-        for key, value in entry.items()
-    }
-    for entry in response
-]
+def etl_json_to_polarsdf(endpoint: str) -> pl.DataFrame:
+    """
+    Performs an HTTP GET request on the given endpoint, retrieves the JSON
+    data and transforms it into a Polars DataFrame with its own format.
 
-# Results in a Polars DataFrame
-df = pl.DataFrame(cleaned_response)
+    Args:
+        endpoint (str): API URL returning a JSON.
+
+    Returns:
+        pl.DataFrame: Polars DataFrame containing the transformed data.
+    """
+
+    # Get request
+    response = requests.get(endpoint).json()
+
+    # Convert all in str
+    cleaned_response = [
+        {
+            key: str(value) if not isinstance(value, (int, float)) else value
+            for key, value in entry.items()
+        }
+        for entry in response
+    ]
+    # Results in a Polars DataFrame
+    df = pl.DataFrame(cleaned_response)
+
+    return df
+
+
+df_etl = etl_json_to_polarsdf(endpoint=url)
 
 # Writing parquet
 path = prepro_data_path + "/trajectoires_beneficiares_minimas_sociaux.parquet"
-df.write_parquet(path)
+df_etl.write_parquet(path)
